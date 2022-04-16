c941b91ccd1e08c873ad7031d83626204614c5c6
Kevin Y. Chen <kyc2915@gmail.com>
2022-04-16 13:16:16 -0700

    draw outer block

--- rendering.js
+++ rendering.js
@@ -12,14 +12,6 @@ function beltramiKleinProjection(r, θ) {
     return [WIDTH / 2 + HEIGHT / 2 * tanh(r) * cos(θ), HEIGHT / 2 - HEIGHT / 2 * tanh(r) * sin(θ)];
 }
 
-function drawLine(ctx, x1, y1, x2, y2, color) {
-    ctx.strokeStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-}
-
 function render(ctx, gameMap, locationMap, animatingStep) {
     const { p, blocks } = gameMap;
 
@@ -29,51 +21,76 @@ function render(ctx, gameMap, locationMap, animatingStep) {
         return [x, y];
     }
 
+    const polygons = [];
+    const processedBlocks = new Set();
+
     // heading is toward the first neighbor of node
-    function drawBlock(block, nodeIndex, heading, transformPointFunc) {
+    function processBlock(block, nodeIndex, heading, transformPointFunc) {
         const { q, nodes } = block;
-        const { D, R, S } = getParameters(p, q);
+        const { D, S } = getParameters(p, q);
+
+        let centerPosition = undefined;
+
+        if (processedBlocks.has(block)) {
+            return;
+        }
+        processedBlocks.add(block);
 
         function getPolygon(center_r, center_θ, heading) {
             const points = [];
             for (let i = 0; i < p; i++) {
-                const [r, θ, localHeading] = move(center_r, center_θ, R, heading + 2 * π * (i + .5) / p);
-                for (let step = 0; step <= NUM_STEPS; step++) {
-                    points.push(equidistantProjection(...transformPointFunc(
-                        ...move(r, θ, S * step / NUM_STEPS, localHeading + π - π / q))));
+                const [r, θ, localHeading] = move(center_r, center_θ, D / 2, heading + 2 * π * i / p);
+
+                const sideIsFar = transformPointFunc(r, θ)[0] > 10;
+                const cornerIsFar = transformPointFunc(...move(r, θ, S / 2, localHeading + π / 2))[0] > 10;
+                for (let step = -NUM_STEPS / 2; step <= NUM_STEPS / 2; step++) {
+                    let d = S * Math.abs(step / NUM_STEPS);
+                    if (sideIsFar) {
+                        d = 0;
+                    }
+                    // render more points near the valid parts, for a better interpolated curve
+                    else if (cornerIsFar) {
+                        d /= 7;
+                    }
+                    points.push(equidistantProjection(
+                        ...transformPointFunc(...move(r, θ, d, localHeading + π / 2 * Math.sign(step)))));
                 }
             }
             return points;
         }
 
         // heading is toward the first neighbor of node
-        function drawNode(center_r, center_θ, heading, node) {
+        function processNode(center_r, center_θ, heading, node) {
             const points = getPolygon(center_r, center_θ, heading);
             if (animatingStep === 0) {
                 locationMap.set(node.coordinate, points);
             }
             const prevPoints = locationMap.get(node.coordinate) || points;
-            ctx.strokeStyle = block.color;
-            ctx.fillStyle = node.contents.type === 'Wall' ? block.color : 'transparent';
-            ctx.beginPath();
+            const interpolatedPoints = [];
             for (let i = 0; i < points.length; i++) {
-                ctx.lineTo(...interpolate(points[i], prevPoints[i]));
+                interpolatedPoints.push(interpolate(points[i], prevPoints[i]));
             }
-            ctx.stroke();
-            ctx.fill();
+            polygons.push({
+                points: interpolatedPoints,
+                strokeStyle: block.color,
+                fillStyle: node.contents.type === 'Wall' ? block.color : 'transparent',
+            });
 
             if (node.contents.type === 'Block') {
                 const block = blocks[node.contents.index];
                 // R_0 is the ratio of length of a square in the Beltrami-Klein projection of this block,
                 // to the length of the smallest square containing the projection of the entire block.
                 const R_0 = tanh((block.minRadius + .5) * D) / tanh(D / 2);
-                drawBlock(block, 0, heading, (r, θ) => transformPointFunc(
+                processBlock(block, 0, heading, (r, θ) => transformPointFunc(
                     ...move(center_r, center_θ, atanh(tanh(r) / R_0), θ + 2 * π * node.facingNeighborIndex / p)));
             }
         }
 
         function helper(nodeIndex, r, θ, heading, prevNodeIndex) {
-            drawNode(r, θ, heading, nodes[nodeIndex]);
+            if (nodeIndex === 0) {
+                centerPosition = [r, θ, heading];
+            }
+            processNode(r, θ, heading, nodes[nodeIndex]);
             for (let i = 0; i < p; i++) {
                 const neighbor = nodes[nodeIndex].neighbors[i];
                 if (!neighbor?.isMainEdge) {
@@ -84,17 +101,39 @@ function render(ctx, gameMap, locationMap, animatingStep) {
                     continue;
                 }
                 const [new_r, new_θ, newHeading] = move(r, θ, D, heading + 2 * π * i / p);
-                // drawLine(ctx, ...equidistantProjection(r, θ), ...equidistantProjection(new_r, new_θ), 'red');
                 helper(newNodeIndex, new_r, new_θ, newHeading - 2 * π * newReturnNeighborIndex / p + π, nodeIndex);
             }
         }
         helper(nodeIndex, 0, 0, heading, -1);
+        return centerPosition;
     }
 
     const { blockIndex, nodeIndex } = blocks.find(block => block.player).parentNode;
     const parentBlock = blocks[blockIndex];
 
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
     // player block 0 always faces east on the screen
-    drawBlock(parentBlock, nodeIndex, -2 * π * parentBlock.nodes[nodeIndex].facingNeighborIndex / p, (r, θ) => [r, θ]);
+    const [center_r, center_θ, centerHeading] = processBlock(
+        parentBlock, nodeIndex, -2 * π * parentBlock.nodes[nodeIndex].facingNeighborIndex / p, (r, θ) => [r, θ]);
+
+    if (parentBlock.parentNode !== undefined) {
+        // TODO this might require taking an arbitrarily high ancestor block
+        const { blockIndex, nodeIndex } = parentBlock.parentNode;
+        const grandparentBlock = blocks[blockIndex];
+        const { D } = getParameters(p, parentBlock.q);
+        const R_0 = tanh((parentBlock.minRadius + .5) * D) / tanh(D / 2);
+        processBlock(grandparentBlock, nodeIndex, -2 * π * grandparentBlock.nodes[nodeIndex].facingNeighborIndex / p,
+            (r, θ) => move(center_r, center_θ, atanh(Math.min(R_0 * tanh(r), 1 - ɛ)), centerHeading + θ));
+    }
+
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    for (const { points, strokeStyle, fillStyle } of polygons) {
+        ctx.strokeStyle = strokeStyle;
+        ctx.fillStyle = fillStyle;
+        ctx.beginPath();
+        for (const [x, y] of points) {
+            ctx.lineTo(x, y);
+        }
+        ctx.stroke();
+        ctx.fill();
+    }
 }
